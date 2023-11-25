00729fc6af91b4139a7ce2c422a0d5822b938d40
Oliver Wolf <oliver.wolf@study.thws.de>
2023-11-25 17:41:23 +0100

    umbau der studie, hinzufügen einer besseren likert skala

--- src/main/java/com/example/springboot/controllers/HelloController.java
+++ src/main/java/com/example/springboot/controllers/HelloController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
+import java.sql.Connection;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -27,10 +28,6 @@ public class HelloController {
 	@Autowired
 	private IPersonRepository personRepository;
 
-	Person currentPerson;
-	Long currentPersonId;
-
-
 	@Value("${spring.application.name}")
 	String appName;
 
@@ -38,39 +35,6 @@ public class HelloController {
 	public String showStartPage(Model model) {
 		model.addAttribute("appName", appName);
 		return "startPage";
-	}
-
-	@RequestMapping(value = "/person", method = RequestMethod.GET)
-	public String showPersonPage(Model model) {
-		List<Education> educations = educationRepository.findAll();
-		model.addAttribute("educations", educations);
-
-		List<EducationDirection> educationDirections = educationDirectionRepository.findAll();
-		model.addAttribute("educationDirections", educationDirections);
-
-		return "personalInfos";
-	}
-
-	@RequestMapping(value = "savePerson", method = RequestMethod.POST)
-	public String saveUxData(@RequestParam(value = "age", required = false) Integer age,
-							 @RequestParam("educationId") Long educationId,
-							 @RequestParam("educationDirectionId") Long educationDirectionId) {
-		Optional<Education> educationOpt = educationRepository.findById(educationId);
-		Optional<EducationDirection> educationDirectionOpt = educationDirectionRepository.findById(educationDirectionId);
-
-		currentPerson = new Person();
-		currentPerson.setAge(age);
-		currentPerson.setTestGroup(0); // traditional -> redirect to testPageA
-		//currentPerson.setTestGroup(1); // scrollytelling -> redirect to testPageB
-		if(educationOpt.isPresent() && educationDirectionOpt.isPresent()) {
-			currentPerson.setEducation(educationOpt.get());
-			currentPerson.setEducationDirection(educationDirectionOpt.get());
-		}
-		Person newPerson = personRepository.saveAndFlush(currentPerson);
-		currentPersonId = newPerson.getId();
-
-		return "redirect:testPageA"; // traditional -> redirect to testPageA
-		//return "redirect:testPageB"; // scrollytelling -> redirect to testPageB
 	}
 
 	@RequestMapping(value = "/testPageA", method = RequestMethod.GET)
@@ -87,32 +51,51 @@ public class HelloController {
 
 	@RequestMapping(value = "/survey", method = RequestMethod.GET)
 	public String showSurvey(Model model) {
-		if(currentPerson == null) {
-			List<Person> persons = personRepository.findAll();
-			Optional<Date> lastEntry = persons.stream().map(Person::getCreatedAt).max(Date :: compareTo);
-			currentPerson = persons.stream().filter(p -> p.getCreatedAt() == lastEntry.get()).findFirst().get();
-		}
+		List<Education> educations = educationRepository.findAll();
+		model.addAttribute("educations", educations);
+
+		List<EducationDirection> educationDirections = educationDirectionRepository.findAll();
+		model.addAttribute("educationDirections", educationDirections);
+
+		Person currentPerson = new Person();
+		//currentPerson.setTestGroup(0); // traditional -> redirect to testPageA
+		currentPerson.setTestGroup(1); // scrollytelling -> redirect to testPageB
 		model.addAttribute("person", currentPerson);
 		return "survey";
 	}
 
-	@RequestMapping(value = "/saveUxData", method = RequestMethod.POST)
-	public String saveUxData(@RequestParam(value = "value1", required = false) Integer value1,
-							 @RequestParam(value = "value2", required = false) Integer value2,
-							 @RequestParam(value = "value3", required = false) Integer value3) {
+	@RequestMapping(value = "/saveData", method = RequestMethod.POST)
+	public String saveData(@RequestParam(value = "age", required = false) Integer age,
+						  @RequestParam("educationId") Long educationId,
+						  @RequestParam("educationDirectionId") Long educationDirectionId,
+						  @RequestParam(value = "value1", required = false) Integer value1,
+						  @RequestParam(value = "value2", required = false) Integer value2,
+						  @RequestParam(value = "value3", required = false) Integer value3) {
+
+		Optional<Education> educationOpt = educationRepository.findById(educationId);
+		Optional<EducationDirection> educationDirectionOpt = educationDirectionRepository.findById(educationDirectionId);
+
+		Person currentPerson = new Person();
+		currentPerson.setAge(age);
+		//currentPerson.setTestGroup(0); // traditional -> redirect to testPageA
+		currentPerson.setTestGroup(1); // scrollytelling -> redirect to testPageB
+		if(educationOpt.isPresent() && educationDirectionOpt.isPresent()) {
+			currentPerson.setEducation(educationOpt.get());
+			currentPerson.setEducationDirection(educationDirectionOpt.get());
+		}
 
 		// save user experience data to database
 		currentPerson.setValue1(value1);
 		currentPerson.setValue2(value2);
 		currentPerson.setValue3(value3);
-		personRepository.save(currentPerson);
+
+		Person newPerson = personRepository.saveAndFlush(currentPerson);
 
 		return "redirect:goodbye";
 	}
 
 	@RequestMapping(value = "/goodbye", method = RequestMethod.GET)
 	public String showEndPage(Model model) {
-		model.addAttribute("person", currentPerson);
 		return "endPage";
 	}
 }
